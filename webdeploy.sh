59119d51d0c3fd5ee804345e2840f4d87fd4f11b
ravi <ravi.kandhadai@epfl.ch>
2015-08-21 15:08:53 +0200

    Removing script files for copying libraries

--- webdeploy.sh
+++
@@ -1,5 +0,0 @@
-cp target/scala-2.11/cfgchecker_2.11-0.1.jar ../grammar-web/lib/
-cp lib/antlr-4.5-complete.jar ../grammar-web/lib/
-cp exercises-data/GrammarDatabase.xml ../grammar-web/public/resources/
-cp exercises-data/*.gram ../grammar-web/public/resources/
-echo "4 files copied"
