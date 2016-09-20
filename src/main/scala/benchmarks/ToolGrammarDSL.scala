b8431e4044f007720d91cc7cc84f17b9b5b1994f
ravi <ravi.kandhadai@epfl.ch>
2016-09-20 20:11:20 +0200

    Minor cleaning up

--- src/main/scala/benchmarks/ToolGrammarDSL.scala
+++ src/main/scala/benchmarks/ToolGrammarDSL.scala
@@ -37,25 +37,4 @@ object ToolGrammarDSL extends Benchmark {
       | "!" ~ 'Expression | "(" ~ 'Expression ~ ")" ),
     'Identifier -> "IDENTIFIER"     
   ))
-    
-    /*bnfgrammar"""Goal	::=	MainObject ( ClassDeclaration )* 
-MainObject	::=	object Identifier { def main '(' ')' : Unit = { ( Statement )* } }
-ClassDeclaration	::=	class Identifier ( extends Identifier )? { ( VarDeclaration )* ( MethodDeclaration )* }
-VarDeclaration	::=	var Identifier : Type ;
-MethodDeclaration	::=	def Identifier '(' ( Identifier : Type ( , Identifier : Type )* )? ')' : Type = { ( VarDeclaration )* ( Statement )* return Expression ; }
-Type	::=	Int [ ] | Bool | Int | String | Identifier
-Statement	::=	{ ( Statement )* } 
-  | if '(' Expression ')' Statement ( else Statement )? 
-  | while '(' Expression ')' Statement 
-  | println '(' Expression ')' ; 
-  | Identifier = Expression ; 
-  | Identifier [ Expression ] = Expression ;
-Expression	::=	Expression ( && | '||' | == | < | '+' | - | '*' | / ) Expression 
-  | Expression [ Expression ] 
-  | Expression . length 
-  | Expression . Identifier '(' ( Expression ( , Expression )* )? ')' 
-  | INTEGER_LITERAL | " STRING_LITERAL " | true | false 
-  | Identifier | this | new Int [ Expression ] | new Identifier '(' ')' 
-  | ! Expression | '(' Expression ')'
-Identifier	::=	IDENTIFIER"""*/
 }
